a731f8c1d14f33d5f81ab59039e099c0369487d0
Jeongseop <sotrjs@gmail.com>
2025-10-14 23:04:52 +0900

    feat: Add Mongoose and connect to MongoDB Atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
+// src/app.module.ts
+
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import 'dotenv/config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.DATABASE_URL!), // Add '!' here
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
